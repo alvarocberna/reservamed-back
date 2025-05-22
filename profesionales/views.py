290336305bf775f77ca9f3aadcf82858a8adedc3
AlvaroACB <alv.canete@duocuc.cl>
2025-05-21 21:00:51 -0400

    component profesional actualizado

--- profesionales/views.py
+++ profesionales/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 print('hola desde el componente profesionales')
 
-print('este es otro print')
+print('este es otro print el cul vmos a usar pr este ejercicio')
+
+print('tenemos otro print mas de ejemplo wuuuuu')
 
 # Create your views here.
