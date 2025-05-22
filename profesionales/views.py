2f43fb067031b58bbd6a7bd227ea707d63fc2499
Alvaro <cberna.alvaro@gmail.com>
2025-05-21 21:17:00 -0400

    conflitos 2

--- profesionales/views.py
+++ profesionales/views.py
@@ -4,6 +4,6 @@ print('hola desde el componente profesionales')
 
 print('este es otro print wooooo')
 
-print('vamos a generar conflictoooos')
+print('vamos a generar conflictoo58903490890oos')
 
 # Create your views here.
