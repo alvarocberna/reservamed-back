4bb693a975442299fbba00766b73b4b39999b7a3
Alvaro <cberna.alvaro@gmail.com>
2025-05-21 21:22:49 -0400

    nuevas actualizaciones

--- profesionales/views.py
+++ profesionales/views.py
@@ -4,9 +4,13 @@ print('hola desde el componente profesionales')
 
 print('este es otro print wooooo')
 
+<<<<<<< Updated upstream
 print('tenemos otro print mas de ejemplo wuuuuu')
 
 print('vamos a generar conflictoooffhwejfnwejkfnefjkos')
 print('vamos a generar conflictoo58903490890oos')
+=======
+print('vamos a generar conflictooouo3438503759032os')
+>>>>>>> Stashed changes
 
 # Create your views here.
